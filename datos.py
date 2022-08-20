48aa781f77c1f787a3101c84cf0af74ec8f3105b
kevs256 <kevin.quintana.256bit@gmail.com>
2022-08-19 23:29:44 -0500

    casi todo, detalles, dios ayudame :CCCCCC

--- datos.py
+++ datos.py
@@ -1,9 +1,13 @@
+from audioop import rms
 import imghdr
+from logging.handlers import DatagramHandler
 from pydoc import render_doc
 from re import template
 from tkinter import Canvas
 import pandas as pd
 import matplotlib.pyplot as plot
+import matplotlib
+matplotlib.use('Agg')
 import numpy as np
 from scipy import stats
 # conversor de imagen
@@ -26,7 +30,7 @@ from sklearn.metrics import mean_squared_error
 # funciones del tratamiento de datos, toco ponerlas aca porque no las encontraba
 #decimos ubicacion del csv
 file = "datos/abalone.csv"
-
+rmse=''
 #leemos el fichero
 datacsv = pd.read_csv(file)
 
@@ -86,6 +90,7 @@ def deleting(column, todelete):
 ##este metodo se encarga de mostrar el antes y despues de el historigrama
 #recibe el dataframe modificado (limpio), la columna a tratar y el dataframe original
 def framehist(datacsvframe, column, dataframeoriginal):
+    fig = plot.figure()
     figura = plot.hist(datacsvframe[column])
     ubicacion = 'datos/static/grafica.png'
     plot.savefig(ubicacion)
@@ -99,6 +104,7 @@ def framehist(datacsvframe, column, dataframeoriginal):
 
 ##este metodo se encarga de generar el antes y despues de el diagrama de bigotes
 def frameboxplot(datacsvframe, column, dataframeoriginal):
+    fig = plot.figure()
     figura = plot.boxplot(datacsvframe[column])
     ubicacion = 'datos/static/grafica.png'
     plot.savefig(ubicacion)
@@ -137,6 +143,7 @@ def corazon():
 #el codigo cambia ligeramente
 #por ahora solo la imagen
 def framescatter(datacsvframeA,datacsvframeB, column,column2, dataframeoriginal):
+    fig = plot.figure()
     plot.scatter(list(datacsvframeA),list(datacsvframeB))
     ubicacion = 'datos/static/grafica.png'
     plot.savefig(ubicacion)
@@ -147,10 +154,7 @@ def framescatter(datacsvframeA,datacsvframeB, column,column2, dataframeoriginal)
 
 #pal modelo
 def framescattermodel(modelogeneradolimpio, modelogeneradosucio, variablelimpia, variablesucia):
-    print("aca maricaaaaaaaaaa el variable sucia")
-    print(variablesucia)
-    print("aca maricaaaaaaaaaa el modelo sucia")
-    print(modelogeneradosucio)
+    fig = plot.figure()
     plot.scatter(modelogeneradolimpio, variablelimpia)
     ubicacion = 'datos/static/grafica.png'
     plot.savefig(ubicacion)
@@ -180,19 +184,32 @@ def construirnuevalimpia(modelo,alimpiar):
 #funciones para el modelo
 #modelo de una entrada y una salida, con regresion lineal (porque no hemos visto mas)
 def modelounoauno(tamañoentrenamiento,X,Y):
-    X_train, X_test, Y_train, Y_test = train_test_split(
+    X_train, X_test, y_train, y_test = train_test_split(
                                         X,
                                         Y,
                                         train_size   = corregirentrenamiento(tamañoentrenamiento),
                                     )
     modelo = LinearRegression()
-    modelo.fit(X = np.array(X_train).reshape(-1, 1), y = Y_train)
+    modelo.fit(X = np.array(X_train).reshape(-1, 1), y = y_train)
     datosobtenidos = modelo.predict(X = np.array(X_test).reshape(-1,1))
-    rmse = mean_squared_error(y_true  = Y_test, y_pred  = datosobtenidos)
-    print(rmse)
+    rmse = mean_squared_error(y_true  = y_test, y_pred  = datosobtenidos)
     return datosobtenidos
     
-
+#modelo uno a muchos
+#x es una lista
+def modelounoamuchos(tamañoentrenamiento,X,Y):
+    X = X.drop(['sex'], axis=1)
+    Y = Y[0:len(X)]
+    X_train, X_test, y_train, y_test = train_test_split(
+                                        X,
+                                        Y,
+                                        train_size   = corregirentrenamiento(tamañoentrenamiento),
+                                    )
+    modelo = LinearRegression()
+    modelo.fit(X = np.array(X_train), y = y_train)
+    datosobtenidos = modelo.predict(X = np.array(X_test))
+    rmse = mean_squared_error(y_test,datosobtenidos)
+    return datosobtenidos
 
 #funcion de correcion de tamañode entranamiento
 def corregirentrenamiento(tamañoentrenamiento):
@@ -227,7 +244,7 @@ def visualize():
     # devuelve el texto
 
     #esto recupera que quiere la persona, modelo o scatter directamente
-    modelo = request.form['modelo']
+    modelo = request.form['modelo2']
     #retorna si, no
 
     # regresion, se hace igual que arriba
@@ -258,6 +275,7 @@ def visualize():
     if (len(variableentrada) == 1 and variablesalida != "no"):
         if(modelo=="si"):
             if (atipicos == "activado"):
+                #este bloque, esta raro, creo que a eso se debe la imagen, hay que revisarlo
                 muchasJuntas=variableentrada
                 muchasJuntas.append(variablesalida)
                 modelogeneradolimpio = modelounoauno(int(tamañoentrenamiento),deleting(variableentrada[0], todeletemodel(muchasJuntas, corregirfactoralfa(factoralfa)))[variableentrada[0]],deleting(variablesalida, todeletemodel(muchasJuntas, corregirfactoralfa(factoralfa)))[variablesalida])
@@ -275,9 +293,32 @@ def visualize():
             framescatter(deleting(variableentrada[0],todeletemodel(muchasJuntas, corregirfactoralfa(factoralfa)))[variableentrada[0]],deleting(variablesalida, todeletemodel(muchasJuntas, corregirfactoralfa(factoralfa)))[variablesalida],variableentrada[0],variablesalida,datacsv)
             if (atipicos == "desactivado"):
                 corazon()
+    else:
+        if (atipicos == "activado" and variablesalida !="no"):
+            #x es datafream
+            #y es una columna
+            variableentrada.append(variablesalida)
+            filasaeliminar=todeletemodel(variableentrada,factoralfa)
+            resultadoDF=deleting("ola",filasaeliminar)
+            modelogeneradolimpio =modelounoamuchos(corregirentrenamiento(int(tamañoentrenamiento)),resultadoDF,deleting("ola",todelete(variablesalida,quantiles(variablesalida),corregirfactoralfa(factoralfa)))[variablesalida])
+            modelogeneradosucio =modelounoamuchos(corregirentrenamiento(int(tamañoentrenamiento)),datacsv,datacsv[variablesalida])
+            variablelimpia=deleting("ola",todelete(variablesalida,quantiles(variablesalida),corregirfactoralfa(factoralfa)))[variablesalida]
+            if(len(modelogeneradolimpio)>len(variablelimpia)):
+                modelogeneradolimpio = modelogeneradolimpio[0:len(variablelimpia)]
+            else:
+                variablelimpia = variablelimpia[0:len(modelogeneradolimpio)]
+            listakk = list(datacsv[variablesalida])
+            if(len(datacsv[variablesalida])>len(modelogeneradosucio)):
+                listakk = list(datacsv[variablesalida])
+                listakk=listakk[0:len(modelogeneradosucio)]
+            else:
+                modelogeneradosucio = modelogeneradosucio[0:len(datacsv[variablesalida])]
+            framescattermodel(modelogeneradolimpio,modelogeneradosucio,variablelimpia,listakk)
+        else:
+            corazon()
     #con este return, solo le digo que renderice con las 2 imagenes cuales quiera
     #ya depende de cada metodo de los de arriba de las rutas de flask guardar las imagenes que son
-    return render_template('index.html', datas=["../static/grafica2.png", "../static/grafica.png"])
+    return render_template('index.html', datas=["../static/grafica2.png", "../static/grafica.png",rmse])
 
 
 if __name__ == '__main__':
